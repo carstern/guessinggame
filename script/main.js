3fb7360dd8fca6e454ed5388c1b9c49dbbf02a27
Caroline Edgren <caroline.edgren88@gmail.com>
2023-11-15 11:57:41 +0100

    Made som functionality changes

--- script/main.js
+++ script/main.js
@@ -1,69 +1,92 @@
 //Skapa kopplingar till html
-//antal gissningar kvar
-const guessesLeft = document.getElementById("guesses-left");
-//starta spelet
+
+//Knapp för att starta spelet
 const startGame = document.getElementById("startGame");
-const gameDiv = document.getElementById("game");
-//guess input
+//Fält där gissningen skrivs
 let guessInput = document.getElementById("guess");
-//guess-btn
+//Knapp för att submitta gissning
 const guessBtn = document.getElementById("guessBtn");
 //guess list
 const guessList = document.getElementById("guessList");
-//visa antalet gissningar
-let numberOfGuesses;
+//Textinfo med antal gissningar kvar
+const guessesLeft = document.getElementById("guesses-left");
 
-//Generera en random siffra mellan 0-100 med mat random vid start
+
 //lagra siffran
 let correctAnswer;
 let userGuess;
+//Antalet gissningar
+let numberOfGuesses;
+
+// Lås inputfältet innan start
+guessInput.setAttribute("disabled", true);
+
+//Generera en random siffra mellan 0-100 med math random vid klick på start
 startGame.addEventListener("click", function () {
   correctAnswer = Math.floor(Math.random() * 101);
-  console.log(correctAnswer); // skriv ut korrekt siffra i log
-  // töm listan vid omstart
-  guessList.textContent = ''; 
+  console.log(correctAnswer); //skriv ut korrekt siffra i loggen
+  // Töm listan vid klick på start
+  feedback.innerHTML = '';
   numberOfGuesses = 5;
-  //presentera antal gissningar kvar
-  guessesLeft.textContent = `You have ${numberOfGuesses} guesses left!`;
+  guessInput.value = '';
+  guessBtn.removeAttribute("disabled");
+  guessInput.removeAttribute("disabled");
+  //Presentera antal gissningar
+  guessesLeft.textContent = `You have ${numberOfGuesses} guesses!`;
   });
 
-//skapa eventlistener på knappen
+//Submitta en gissning
 guessBtn.addEventListener("click", function () {
-  //hämta gissning från input
+  //hämta gissning från inputfältet
   let userGuess = guessInput.value;
-  console.log(userGuess);
-
-  //jämför input med mathimport
+  //jämför input med correctAnser från startGame 
   if (userGuess < correctAnswer) {
     console.log("Too low!");
     //minska antalet gissningar kvar
     numberOfGuesses--;
     guessesLeft.textContent = `You have ${numberOfGuesses} guesses left!`;
     //ge feedback i html
-    //display tidigare gissningar
     const feedbackElement = document.createElement('li');
     feedbackElement.textContent = `${userGuess} is to low, try again!`;
     feedback.appendChild(feedbackElement);
   } else if (userGuess == correctAnswer){
     console.log("Correct!");
+    //minska antalet gissningar kvar
     numberOfGuesses--;
     guessesLeft.textContent = `You have ${numberOfGuesses} guesses left!`;
+    //ge feedback i html
     const feedbackElement = document.createElement('li');
     feedbackElement.textContent = `${userGuess} is correct, well done!`;
     feedback.appendChild(feedbackElement);
   } else if (userGuess > correctAnswer) {
     console.log("Too high!");
+    //minska antalet gissningar kvar
     numberOfGuesses--;
     guessesLeft.textContent = `You have ${numberOfGuesses} guesses left!`;
+    //ge feedback i html
     const feedbackElement = document.createElement('li');
     feedbackElement.textContent = `${userGuess} is too high`;
     feedback.appendChild(feedbackElement);
-  } if (numberOfGuesses == 0){
+  } 
+  
+  if (numberOfGuesses == 0){
         guessesLeft.textContent = `Game Over!`;
-        gameDiv.style.backgroundColor = black;
+        guessBtn.setAttribute("disabled", true);
+        //skapa knapp för omstart av spelet
+        const feedbackElement = document.createElement('li');
+        feedbackElement.textContent = `Game is over, try again!`;
+        feedback.appendChild(feedbackElement);
+        guessInput.setAttribute("disabled", true);
   }
-//Fusk-knapp som visas när man anger 666
-  if (userGuess == 666) {
+
+//Extra - for funzies
+//Fusk-knapp som visas när man anger specifik siffra
+if(numberOfGuesses == 2){
+  const hint = document.createElement('p');
+  hint.textContent = "Psst, type 37!";
+  feedback.appendChild(hint);
+}
+  if (userGuess == 37) {
     const tryAgainElement = document.createElement('button');
     tryAgainElement.textContent = `Hellraiser!`;
     feedback.appendChild(tryAgainElement);
@@ -74,11 +97,7 @@ guessBtn.addEventListener("click", function () {
         cheatElement.textContent = correctAnswer;
         feedback.appendChild(cheatElement);
     })
+    
 }
 
 });
-
-// const feedback = document.quarySelector("p");
-//när gissningarna är slut, avsluta spelet
-
-
